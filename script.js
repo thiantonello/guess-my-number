8e1a5633f0c2b9c1cd9a23941efef3821ac393f2
Thiago Antonello Vargas <thiantonello@gmail.com>
2023-01-27 17:27:22 -0300

    feat: add game logic

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-'use strict';
+"use strict";
+
+/*
+console.log(document.querySelector(".message").textContent);
+console.log(document.querySelector(".score").textContent);
+console.log(document.querySelector(".highscore").textContent);
+
+document.querySelector(".message").textContent = "🎊 ¡Correct Number! 🎊";
+
+document.querySelector(".secret-number").textContent = 50;
+document.querySelector(".score").textContent = 95;
+document.querySelector(".highscore").textContent = 999;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 50) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (score > 0) {
+    if (!guess) {
+      document.querySelector(".message").textContent = `⛔️ No number!`;
+    } else if (guess === secretNumber) {
+      document.querySelector(".message").textContent = "🎊 ¡Correct Number! 🎊";
+      document.querySelector(".secret-number").textContent = secretNumber;
+
+      highScore = score;
+
+      document.querySelector(".highscore").textContent = highScore;
+    } else if (guess > secretNumber) {
+      if (guess > secretNumber + 10) {
+        document.querySelector(
+          ".message"
+        ).textContent = `⬆️⬆️⬆️ Too high! ⬆️⬆️⬆️ `;
+        score--;
+      } else {
+        document.querySelector(".message").textContent = `⬆️ High! ⬆️`;
+        score--;
+      }
+    } else if (guess < secretNumber) {
+      if (guess < secretNumber - 10) {
+        document.querySelector(
+          ".message"
+        ).textContent = `⬇️⬇️⬇️ Too low! ⬇️⬇️⬇️`;
+        score--;
+      } else {
+        document.querySelector(".message").textContent = `⬇️ Low! ⬇️`;
+        score--;
+      }
+    }
+  }
+
+  document.querySelector(".score").textContent = score;
+
+  if (score === 0) {
+    document.querySelector(".message").textContent = `You loose... 😔`;
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 50) + 1;
+  score = 20;
+
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = `Start guessing...`;
+  document.querySelector(".secret-number").textContent = "?";
+  document.querySelector(".guess").value = false;
+  document.querySelector(".guess").textContent = "?";
+});
